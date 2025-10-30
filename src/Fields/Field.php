6184335df3f6dd43f413da64194259516307f8d7
Ahmet Cangir <ahmetcangir07@gmail.com>
2025-10-30 11:35:14 +0100

    feat: static style (Field::text etc.)

--- src/Fields/Field.php
+++ src/Fields/Field.php
@@ -14,4 +14,40 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Field builder (alias for FieldBuilder).
  */
-class Field extends FieldBuilder {}
+class Field extends FieldBuilder {
+	/**
+	 * Create a builder via static typed constructor, e.g. Field::text('id').
+
+	 * @param string $name      Called static method name (used as type).
+	 * @param array  $arguments Arguments passed to the method (expects [ id ]).
+	 * @return static
+	 * @throws \InvalidArgumentException If ID is missing.
+	 */
+	public static function __callStatic( string $name, array $arguments ) {
+		$id = '';
+		if ( isset( $arguments[0] ) && is_string( $arguments[0] ) ) {
+			$id = $arguments[0];
+		}
+
+		if ( '' === $id ) {
+			throw new \InvalidArgumentException( 'Field id is required for static constructor.' );
+		}
+
+		$type = strtolower( $name );
+		// Allow snake_case in static calls (maps to kebab-case types if used).
+		$type = str_replace( '_', '-', $type );
+
+		return new static( $id, $type );
+	}
+
+	/**
+	 * Explicit factory helper.
+	 *
+	 * @param string $id   Field id.
+	 * @param string $type Field type.
+	 * @return static
+	 */
+	public static function make( string $id, string $type ) {
+		return new static( $id, $type );
+	}
+}
