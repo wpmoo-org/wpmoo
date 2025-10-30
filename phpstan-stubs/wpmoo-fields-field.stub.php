e956af1589949c4a9d4a7f6c0b3df92a5d90cbc2
Ahmet Cangir <ahmetcangir07@gmail.com>
2025-10-30 12:01:26 +0100

    feat(phpstan): ship central stubs and config (phpstan.neon)

---
+++ phpstan-stubs/wpmoo-fields-field.stub.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PHPStan stub for WPMoo\Fields\Field magic static constructors.
+ * Distributed with the framework so consumers can include it from vendor/.
+ */
+
+namespace WPMoo\Fields;
+
+class Field
+{
+    /** @return self */
+    public static function text(string $id) {}
+
+    /** @return self */
+    public static function textarea(string $id) {}
+
+    /** @return self */
+    public static function color(string $id) {}
+
+    /** @return self */
+    public static function checkbox(string $id) {}
+
+    /** @return self */
+    public static function accordion(string $id) {}
+
+    /** @return self */
+    public static function fieldset(string $id) {}
+}
+
